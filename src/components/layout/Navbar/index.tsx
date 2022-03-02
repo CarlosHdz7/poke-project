ffbe0cd6279fe8b49539540ad54ef9eb5ec1ff52
CarlosHdz7 <carloshdz.udb@gmail.com>
2022-03-02 13:05:03 -0600

    :sparkles: Add navbar component

--- src/components/layout/Navbar/index.tsx
+++ src/components/layout/Navbar/index.tsx
@@ -1,7 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './index.scss';
 
 const Navbar = () => {
-  return <div>index</div>;
-};
+  const [sidebar, setSideBar] = useState<boolean>(false);
 
+  const showSideBar = () => setSideBar(!sidebar);
+
+  return (
+    <>
+      <div className="navbar">
+        <button type="button" className="side-button" onClick={showSideBar}>
+          <i className="bi bi-list" />
+        </button>
+        <div className="navbar-container-options">
+          <Link to="/" className="navbar-container-item">
+            PokeApi
+          </Link>
+          <div className="d-flex">
+            <Link to="pokemons" className="navbar-container-item">
+              Pokemons
+            </Link>
+            <Link to="bookmarks" className="navbar-container-item">
+              Bookmarks
+            </Link>
+            <Link to="bookmarks" className="navbar-container-item">
+              About
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <nav className={sidebar ? 'side-menu-container active' : 'side-menu-container'}>
+        <ul className="side-menu-items">
+          <button type="button" className="side-menu__item--button" onClick={showSideBar}>
+            <i className="bi bi-x-lg" />
+          </button>
+          <li className="side-menu__item">
+            <Link to="/">
+              {/* <i className={icon} /> */}
+              <span>PokeApi</span>
+            </Link>
+          </li>
+          <li className="side-menu__item">
+            <Link to="pokemons">
+              <span>Pokemons</span>
+            </Link>
+          </li>
+          <li className="side-menu__item">
+            <Link to="bookmarks">
+              <span>Bookmarks</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
 export default Navbar;
--- src/views/AppRouter/index.tsx
+++ src/views/AppRouter/index.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Navbar from '../../components/layout/Navbar';
 import BookmarksView from '../BookmarksView';
 import PokemonDetailView from '../PokemonDetailView';
 import PokemonsView from '../PokemonsView';
 
 const AppRouter = () => (
-  <div className="container">
-    <Routes>
-      <Route path="pokemons" element={<PokemonsView />} />
-      <Route path="pokemons/:id" element={<PokemonDetailView />} />
-      <Route path="bookmarks" element={<BookmarksView />} />
-      <Route path="/" element={<PokemonsView />} />
-    </Routes>
-  </div>
+  <>
+    <Navbar />
+    <div className="container">
+      <Routes>
+        <Route path="pokemons" element={<PokemonsView />} />
+        <Route path="pokemons/:id" element={<PokemonDetailView />} />
+        <Route path="bookmarks" element={<BookmarksView />} />
+        <Route path="/" element={<PokemonsView />} />
+      </Routes>
+    </div>
+  </>
 );
 
 export default AppRouter;
