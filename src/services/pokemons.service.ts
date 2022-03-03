eda3b025b14a066e48ff6f72b92b8715d58c8ea5
CarlosHdz7 <carloshdz.udb@gmail.com>
2022-03-02 22:02:24 -0600

    :sparkles: Add searchbar

--- src/services/pokemons.service.ts
+++ src/services/pokemons.service.ts
@@ -1,13 +1,14 @@
 import IPokemon from '../interfaces/IPokemon';
 import { get } from './fetchData';
 
-const getAllPokemons = async () => {
-  const pokemons = await get<IPokemon[]>(`https://poke-api-hdz.herokuapp.com/pokemons`);
+const getAllPokemons = async (filter: string = '') => {
+  const url = filter ? `/pokemons?name_like=${filter}` : '/pokemons';
+  const pokemons = await get<IPokemon[]>(url);
   return pokemons;
 };
 
 const getPokemonById = async (id: number) => {
-  const pokemon = await get<IPokemon>(`https://poke-api-hdz.herokuapp.com/pokemons/${id}`);
+  const pokemon = await get<IPokemon>(`/pokemons/${id}`);
   return pokemon;
 };
 
--- src/views/PokemonsView/index.tsx
+++ src/views/PokemonsView/index.tsx
@@ -6,24 +6,37 @@ import './index.scss';
 
 const PokemonsView = () => {
   const [pokemonsState, setPokemonsState] = useState<IPokemon[]>();
+  const [filterState, setFilterState] = useState('');
 
   useEffect(() => {
     getPokemons();
-  }, []);
+  }, [filterState]);
 
   const getPokemons = async () => {
-    const pokemons = await getAllPokemons();
+    const pokemons = await getAllPokemons(filterState);
     setPokemonsState(pokemons);
+  };
+
+  const handleSearch = (value: string) => {
+    setFilterState(value);
   };
 
   return (
     <>
       <h1 className="title">Pokemons</h1>
+      <input
+        value={filterState}
+        type="text"
+        className="searchbar"
+        placeholder="Search ..."
+        onChange={(e) => handleSearch(e.target.value)}
+      />
       <div className="cards-container">
-        {pokemonsState?.map((pokemon: IPokemon) => (
-          <PokemonCard image={pokemon.image} name={pokemon.name} />
-        ))}
+        {pokemonsState?.length !== 0 &&
+          pokemonsState?.map((pokemon: IPokemon) => <PokemonCard image={pokemon.image} name={pokemon.name} />)}
       </div>
+
+      {!pokemonsState?.length && '...loading'}
     </>
   );
 };
