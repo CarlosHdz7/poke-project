a9c8ba9199f7d8d095c82291d73b1d4cab4ef5b4
CarlosHdz7 <carloshdz.udb@gmail.com>
2022-03-13 20:44:12 -0600

    :fire: Delete unnecessary files

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-import { Provider } from 'react-redux';
-import store from 'store';
-import { BrowserRouter } from 'react-router-dom';
-
-it('renders pokemon list initially', () => {});
